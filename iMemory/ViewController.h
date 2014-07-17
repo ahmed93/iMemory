9a5c20cc26127bfb52e09d9809317e4cca05bbb8
ahmed93 <hero.20112012@gmail.com>
2014-07-17 12:53:51 +0200

    Initial Commit

---
+++ iMemory/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iMemory
+//
+//  Created by Ahmed Mohamed Fareed on 7/17/14.
+//  Copyright (c) 2014 Ahmed Mohamed Fareed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iMemory/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iMemory
+//
+//  Created by Ahmed Mohamed Fareed on 7/17/14.
+//  Copyright (c) 2014 Ahmed Mohamed Fareed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
